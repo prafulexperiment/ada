864e02a225b6ce92e1c83316c1179369d985b43b
prafulexperiment <156900883+prafulexperiment@users.noreply.github.com>
2024-03-13 09:34:04 +0530

    update kruskal1.cpp

--- kruskal1.cpp
+++ kruskal1.cpp
@@ -1,60 +1,64 @@
 #include <iostream>
 using namespace std;
-
-int min1, cost[100][100],parent[100],i,j,x,y,n;
+#include <chrono>
+using namespace chrono;
+// Function prototypes
 void find_min();
-int check_cycle(int x,int y);
-int main()
-{
- int count=0,tot=0,flag=0;
-printf("Enter the number of vertices \n");
- scanf("%d",&n);
- printf("Enter the cost matrix \n");
- printf("Enter 999 for No edges and self loops \n");
- for(i=1;i<=n;i++)
-  for(j=1;j<=n;j++)
-  {
-   scanf("%d",&cost[i][j]);
-   parent[j]=0;
-  }
-  while(count!=n-1 && min1!=999)
-  {
-   find_min();
-   flag=check_cycle(x,y);
-   if(flag==1)
-   {
-    cout<<cost[x][y];
-    count++;
-    tot+=cost[x][y];
-   }
-   cost[x][y]=cost[y][x]=999;
-  }
-  printf("\nThe total cost of minimum spanning tree=%d",tot);
-  return 0;
-  }
+int check_cycle(int x, int y);
 
- void find_min()
- {
-  min1=999;
-  for(i=1;i<=n;i++)
-   for(j=1;j<=n;j++)
-     if(cost[i][j]<min1)
-     {
-      min1=cost[i][j];
-      x=i;
-      y=j;
-     }
-   }
+int min_cost, cost[100][100], parent[100], i, j, x, y, n;
 
-   int check_cycle(int x,int y)
-   {
-    if((parent[x]==parent[y]) && (parent[x]!=0))
-	return 0;
-    else if (parent[x]==0 && parent[y]==0)
-	parent[x]=parent[y]=x;
-    else if(parent[x]==0)
-	parent[x]=parent[y];
-    else if(parent[x]!=parent[y])
-	parent[y]=parent[x];
+int main() {
+    auto start = high_resolution_clock::now();
+    int count = 0, tot = 0, flag = 0;
+    cout<<"Enter the number of vertices \n";
+    cin>>n;
+    cout<<"Enter the cost matrix \n";
+    cout<<"Enter 999 for No edges and self loops \n";
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++) {
+            cin>>cost[i][j];
+            parent[j] = 0;
+        }
+    while (count != n - 1 && min_cost != 999) {
+        find_min();
+        flag = check_cycle(x, y);
+        if (flag == 1) {
+           cout<<x <<"---->"<< y<<"="<<cost[x][y]<<endl;
+           //cout<<endl;
+            count++;
+            tot += cost[x][y];
+        }
+        cost[x][y] = cost[y][x] = 999;
+    }
+    auto end = high_resolution_clock::now();
+   cout<<"\nThe total cost of minimum spanning tree="<< tot;
+//    cout << "Time taken: " << duration_cast<milliseconds>(end - start).count() << " milliseconds" << endl;
+cout << "Time taken: " << (end - start).count() << " microseconds" << endl;
+    return 0;
+    
+    
+}
+
+void find_min() {
+    min_cost = 999;
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
+            if (cost[i][j] < min_cost) {
+                min_cost = cost[i][j];
+                x = i;
+                y = j;
+            }
+}
+
+int check_cycle(int x, int y) {
+    if ((parent[x] == parent[y]) && (parent[x] != 0))
+        return 0;
+    else if (parent[x] == 0 && parent[y] == 0)
+        parent[x] = parent[y] = x;
+    else if (parent[x] == 0)
+        parent[x] = parent[y];
+    else if (parent[x] != parent[y])
+        parent[y] = parent[x];
     return 1;
-   }
+}
